c847067d184724d6fe3ff6bd63bfbb97489aa3d9
dlord <dlord@outlook.com>
2015-08-24 14:21:37 -0400

    Rewriting Slim implementation to offer an API interface

--- www/index.php
+++ www/index.php
@@ -200,50 +200,6 @@ $app->put('/main/posts/:id', function ( $id = null ) use ( $app, $db ) {
 });
 
 /**
-* Modify a post
-* @TODO Add authentification
-* @param $app  Application
-* @param $db   Database connection
-*/
-$app->put('/posts/:id', function ( $id = null ) use ( $app, $db ) {
-    try{
-
-        $data = $app->request()->put();
-        $post = $db->{'posts'}[$id];
-
-        $app->response()->headers->set('Content-Type', 'application/json');
-
-        if( $post ) {
-
-            $timestamp_date = new \DateTime( 'now', new \DateTimeZone('America/Montreal') );
-            $timestamp = $timestamp_date->getTimestamp();
-
-            foreach ($data as $key => $value) {
-                $post[$key] = $value;
-            }
-
-            // If no status is set, data is being modified, and we need to update the modified_timestamp
-            if( empty( $data['status'] ) ) {
-                $post['timestamp_modified'] = $timestamp;
-            }
-
-            $post->update();
-
-            $app->response->setStatus(200);
-            $app->response()->headers->set('Content-Type', 'application/json');
-            echo '{"success":{"text":"Post modified successfully"}}';
-        } else {
-            throw new PDOException('No posts found.');
-        }
-
-    } catch(PDOException $e) {
-        $app->response()->setStatus(404);
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-    die();
-});
-
-/**
 * Fetch a post
 * @TODO Add authentification
 * @param $app  Application
@@ -364,162 +320,6 @@ $app->get('/moderation(/)(:view)', function ( $view = null ) use ( $app, $db ) {
     $app->render('moderation');
 });
 
-// Display after post submit
-$app->get('/submit', function () use ($app) {
-    $app->render('success');
-});
-
-// Submit a post
-$app->post('/submit', function () use ($app, $db) {
-
-    $app->response()->header('Content-Type', 'application/json');
-    $data = $app->request()->post();
-
-    if( empty( $data['timestamp'] ) ){
-        $timestamp_date = new \DateTime( 'now', new \DateTimeZone('America/Montreal') );
-        $timestamp = $timestamp_date->getTimestamp();
-    }
-
-    $author    = empty( $data['author'] ) ? '' : $data['author'];
-    $text      = empty( $data['text'] ) ? '' : $data['text'];
-    $image     = empty( $data['image'] ) ? '' : $data['image'];
-    $type      = "";
-    $file_name = "";
-
-    if( $image === "" ){
-        $image = false;
-    }else{
-        $image = true;
-    }
-
-    $error = false;
-
-    // Generate a unique id for the post
-    $generator = new RandomStringGenerator;
-    $token = $generator->generate(40);
-
-    if( isset( $_FILES['image'] ) && ( isset( $_FILES['image']['name'] ) && $_FILES['image']['name'] ) && ( isset( $_FILES['image']['tmp_name'] ) && $_FILES['image']['tmp_name'] ) ) {
-
-        // File upload properties
-        $base_path    = __DIR__.'/';
-        $upload_path  = 'uploads/';
-        $dir          = $base_path.$upload_path;
-        $max_filesize = 134220000; //128M
-        $finfo         = new finfo(FILEINFO_MIME_TYPE);
-        $mimetypes    = [
-            'image/gif',
-            'image/png',
-            'image/jpeg'
-        ];
-
-        $file_name = $token;
-        $file_data = $_FILES['image'];
-        $file_info = pathinfo( $file_data['name'] );
-        $file_size = filesize( $file_data['tmp_name'] );
-        $file_type = $finfo->file( $file_data['tmp_name'] );
-
-        if (isset($file_info['extension']) && $file_info['extension']) {
-            $file_name .= '.'.$file_info['extension'];
-        }else{
-            $file_name .= '.jpg';
-        }
-
-        $target = $dir.$file_name;
-
-        /**
-        * @TODO
-        * Manage image failures gracefully
-        */
-        if ( !is_writable($dir) ) {
-            throw new Exception('Error: upload directory is not writeable');
-            die();
-        }
-
-        if ( file_exists($target) ) {
-            /**
-            * @TODO
-            * Generate new token? This could mean the token already exists in the database as well
-            */
-            throw new Exception('Error: file already exists');
-            die();
-        }
-
-        if ( !in_array( $file_type, $mimetypes ) ) {
-            throw new Exception('Error: rejected mimetype');
-            die();
-        }
-
-        if ( $file_size > $max_filesize ) {
-            throw new Exception('Error: file too big');
-            die();
-        }
-
-        if( move_uploaded_file( $file_data['tmp_name'], $target) ){
-
-            $imagick = new \Imagick( realpath($target) );
-
-            $exif_data = $imagick->getImageProperties("exif:*");
-
-            if ( ! empty( $exif_data ) && isset( $exif_data['exif:Orientation'] ) && $orientation = $exif_data['exif:Orientation'] ) {
-
-                chmod($target, 0755);
-
-                switch($orientation){
-                    //case '1': // Normal
-                    case '3':// 180 rotate left
-                        $imagick->rotateimage(new \ImagickPixel('none'), 180);
-                    break;
-                    case '6':// 90 rotate right
-                        $imagick->rotateimage(new \ImagickPixel('none'), -90);
-                    break;
-                    case '8':// 90 rotate left
-                        $imagick->rotateimage(new \ImagickPixel('none'), 90);
-                    break;
-                }
-
-                $imagick->writeImage( $target );
-
-            }
-
-            $image = true;
-
-        }else{
-
-            $image = false;
-
-        }
-
-    }
-
-    if( $image === true && $text !== '' ){
-        $type = 'hybrid';
-    }elseif( $image === true && $text === '' ){
-        $type = 'image';
-    }elseif( $image === false && $text !== '' ){
-        $type = 'text';
-    }
-
-    if($author === '' || $type === ''){
-        throw new Exception('Error: empty author or post type');
-        die();
-    }else{
-
-        $post = [
-            'id'        => $token,
-            'timestamp' => $timestamp,
-            'author'    => $author,
-            'text'      => $text,
-            'image'     => $file_name,
-            'status'    => 'moderation',
-            'type'      => $type
-        ];
-
-        $result = $db->posts->insert( $post );
-        $app->redirect('/submit');
-    }
-
-});
-
 // User interface
 $app->get('/', function ( ) use ($app, $db) {
 
@@ -536,6 +336,267 @@ $app->get('/', function ( ) use ($app, $db) {
         'data'       => 'data'
     ]);
     $app->render('user');
+});
+
+/*
+=============================
+
+URL                           HTTP Method  Operation
+/api/v1/posts                 GET          Returns an array of posts
+/api/v1/posts/:id             GET          Returns the contact with id of :id
+/api/v1/posts                 POST         Adds a new contact and return it with an id attribute added
+/api/v1/posts/:id             PUT          Updates the contact with id of :id
+
+=============================
+*/
+
+// API group
+$app->group('/api', function () use ($app, $db) {
+
+    // Version group
+    $app->group('/v1', function () use ($app, $db) {
+
+
+        $app->get('/posts', function ( ) use ($app, $db) {
+
+            $app->response()->headers->set('Content-Type', 'application/json');
+
+            try{
+                $posts = $db
+                    ->posts()
+                    ->where('status','published')
+                    ->order('timestamp DESC');
+
+                if($posts) {
+                    $app->response->setStatus(200);
+                    echo json_encode($posts);
+                } else {
+                    throw new PDOException('No posts found.');
+                }
+
+            } catch(PDOException $e) {
+                $app->response()->setStatus(404);
+                echo '{"error":{"text":'. $e->getMessage() .'}}';
+            }
+            die();
+
+
+        });
+
+        // GET route
+        $app->get('/posts/:id', function ($id) use ($app, $log) {
+
+        });
+
+
+        /**
+        * Modify a post
+        * @TODO Add authentification
+        * @param $app  Application
+        * @param $db   Database connection
+        */
+        $app->post('/submit', function () use ($app, $db) {
+
+            $app->response()->header('Content-Type', 'application/json');
+            $data = $app->request()->post();
+
+            if( empty( $data['timestamp'] ) ){
+                $timestamp_date = new \DateTime( 'now', new \DateTimeZone('America/Montreal') );
+                $timestamp = $timestamp_date->getTimestamp();
+            }
+
+            $author    = empty( $data['author'] ) ? '' : $data['author'];
+            $text      = empty( $data['text'] ) ? '' : $data['text'];
+            $image     = empty( $data['image'] ) ? '' : $data['image'];
+            $type      = "";
+            $file_name = "";
+
+            if( $image === "" ){
+                $image = false;
+            }else{
+                $image = true;
+            }
+
+            $error = false;
+
+            // Generate a unique id for the post
+            $generator = new RandomStringGenerator;
+            $token = $generator->generate(40);
+
+            if( isset( $_FILES['image'] ) && ( isset( $_FILES['image']['name'] ) && $_FILES['image']['name'] ) && ( isset( $_FILES['image']['tmp_name'] ) && $_FILES['image']['tmp_name'] ) ) {
+
+                // File upload properties
+                $base_path    = __DIR__.'/';
+                $upload_path  = 'uploads/';
+                $dir          = $base_path.$upload_path;
+                $max_filesize = 134220000; //128M
+                $finfo         = new finfo(FILEINFO_MIME_TYPE);
+                $mimetypes    = [
+                    'image/gif',
+                    'image/png',
+                    'image/jpeg'
+                ];
+
+                $file_name = $token;
+                $file_data = $_FILES['image'];
+                $file_info = pathinfo( $file_data['name'] );
+                $file_size = filesize( $file_data['tmp_name'] );
+                $file_type = $finfo->file( $file_data['tmp_name'] );
+
+                if (isset($file_info['extension']) && $file_info['extension']) {
+                    $file_name .= '.'.$file_info['extension'];
+                }else{
+                    $file_name .= '.jpg';
+                }
+
+                $target = $dir.$file_name;
+
+                /**
+                * @TODO
+                * Manage image failures gracefully
+                */
+                if ( !is_writable($dir) ) {
+                    throw new Exception('Error: upload directory is not writeable');
+                    die();
+                }
+
+                if ( file_exists($target) ) {
+                    /**
+                    * @TODO
+                    * Generate new token? This could mean the token already exists in the database as well
+                    */
+                    throw new Exception('Error: file already exists');
+                    die();
+                }
+
+                if ( !in_array( $file_type, $mimetypes ) ) {
+                    throw new Exception('Error: rejected mimetype');
+                    die();
+                }
+
+                if ( $file_size > $max_filesize ) {
+                    throw new Exception('Error: file too big');
+                    die();
+                }
+
+                if( move_uploaded_file( $file_data['tmp_name'], $target) ){
+
+                    $imagick = new \Imagick( realpath($target) );
+
+                    $exif_data = $imagick->getImageProperties("exif:*");
+
+                    if ( ! empty( $exif_data ) && isset( $exif_data['exif:Orientation'] ) && $orientation = $exif_data['exif:Orientation'] ) {
+
+                        chmod($target, 0755);
+
+                        switch($orientation){
+                            //case '1': // Normal
+                            case '3':// 180 rotate left
+                                $imagick->rotateimage(new \ImagickPixel('none'), 180);
+                            break;
+                            case '6':// 90 rotate right
+                                $imagick->rotateimage(new \ImagickPixel('none'), -90);
+                            break;
+                            case '8':// 90 rotate left
+                                $imagick->rotateimage(new \ImagickPixel('none'), 90);
+                            break;
+                        }
+
+                        $imagick->writeImage( $target );
+
+                    }
+
+                    $image = true;
+
+                }else{
+
+                    $image = false;
+
+                }
+
+            }
+
+            if( $image === true && $text !== '' ){
+                $type = 'hybrid';
+            }elseif( $image === true && $text === '' ){
+                $type = 'image';
+            }elseif( $image === false && $text !== '' ){
+                $type = 'text';
+            }
+
+            if($author === '' || $type === ''){
+                throw new Exception('Error: empty author or post type');
+                die();
+            }else{
+
+                $post = [
+                    'id'        => $token,
+                    'timestamp' => $timestamp,
+                    'author'    => $author,
+                    'text'      => $text,
+                    'image'     => $file_name,
+                    'status'    => 'moderation',
+                    'type'      => $type
+                ];
+
+                $result = $db->posts->insert( $post );
+                $app->redirect('/submit');
+            }
+
+        });
+
+
+        /**
+        * Modify a post
+        * @TODO Add authentification
+        * @param $app  Application
+        * @param $db   Database connection
+        */
+        $app->put('/posts/:id', function ( $id = null ) use ( $app, $db ) {
+            try{
+
+                $data = $app->request()->put();
+                $post = $db->{'posts'}[$id];
+
+                $app->response()->headers->set('Content-Type', 'application/json');
+
+                if( $post ) {
+
+                    $timestamp_date = new \DateTime( 'now', new \DateTimeZone('America/Montreal') );
+                    $timestamp = $timestamp_date->getTimestamp();
+
+                    foreach ($data as $key => $value) {
+                        $post[$key] = $value;
+                    }
+
+                    // If no status is set, data is being modified, and we need to update the modified_timestamp
+                    if( empty( $data['status'] ) ) {
+                        $post['timestamp_modified'] = $timestamp;
+                    }
+
+                    $post->update();
+
+                    $app->response->setStatus(200);
+                    $app->response()->headers->set('Content-Type', 'application/json');
+                    echo '{"success":{"text":"Post modified successfully"}}';
+                } else {
+                    throw new PDOException('No posts found.');
+                }
+
+            } catch(PDOException $e) {
+                $app->response()->setStatus(404);
+                echo '{"error":{"text":'. $e->getMessage() .'}}';
+            }
+            die();
+        });
+
+        // DELETE route
+        $app->delete('/posts/:id', function ($id) use ($app, $log) {
+
+        });
+
+    });
+
 });
 
 $app->run();
